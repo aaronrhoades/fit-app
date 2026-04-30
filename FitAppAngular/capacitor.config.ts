bafa3e3441f13315d3a53355fa77882732100439
Aaron Rhoades <aaronrhoades91@gmail.com>
2026-04-30 17:21:06 -0400

    Adds Ionic and Capacitor

---
+++ FitAppAngular/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fit.overtone',
+  appName: 'Overtone',
+  webDir: 'dist/fit-app'
+};
+
+export default config;

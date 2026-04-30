a670e31a26e83c30b5a3fbb61974200959183ac4
Aaron Rhoades <aaronrhoades91@gmail.com>
2026-04-30 17:35:24 -0400

    Correction to webDir in capacitor.config.ts

--- FitAppAngular/capacitor.config.ts
+++ FitAppAngular/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'fit.overtone',
   appName: 'Overtone',
-  webDir: 'dist/fit-app'
+  webDir: 'dist/fit-app/browser'
 };
 
 export default config;

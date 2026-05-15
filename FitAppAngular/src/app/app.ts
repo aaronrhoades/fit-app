6808e8e834305141060446f3a4846af25bdbedd1
Aaron Rhoades <aaronrhoades91@gmail.com>
2026-05-15 15:13:45 -0400

    Fix button hover state in android-native builds

--- FitAppAngular/src/app/app.ts
+++ FitAppAngular/src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject, afterNextRender } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-
+import { Platform } from '@ionic/angular';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-root',
   imports: [IonApp, IonRouterOutlet],
@@ -8,5 +9,19 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Overtone');
+  private readonly platform = inject(Platform);
+  private readonly document = inject(DOCUMENT);
+
+  constructor() {
+   afterNextRender(() => {
+      // Platform.ready() returns a promise that resolves once Capacitor is fully loaded
+      this.platform.ready().then(() => {
+        const isAndroidNative = this.platform.is('android') && this.platform.is('capacitor');
+        
+        if (isAndroidNative) {
+          this.document.body.classList.add('android-native');
+        }
+      });
+    });
+  }
 }
